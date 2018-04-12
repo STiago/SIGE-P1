e290e188eeb77670d23520ee5c1da28af8e99d95
STiago <victoriasantiago@correo.ugr.es>
2018-04-12 22:44:52 +0100

    Deleting variables in loans

--- script.R
+++ script.R
@@ -86,7 +86,53 @@ Returns -> False
 Asi que sabiendo que las dos primeras son iguales pasamos a eliminar una de ellas:
 loans <- subset(loans, select= -funded_amnt)
 
-Ahora pasamos a explorar el contenido de las siguientes columnas en busca 
+Ahora pasamos a explorar el contenido de las siguientes columnas y procedemos a eliminar las siguientes ya que no aportan informacion
+Valores muy desiqlibrados: eg: S->114600 N->3 ó  0->114690, 4->1, 13->2, 26->1
+- table(loans$hardship_flag): Donde sale que todas las filas no tienen este plan en su prestamos y solo un caso si N=114804      Y=1
+  loans$hardship_flag <- NULL
+- table(loans$debt_settlement_flag): Ocurre lo mismo que con el anterior y recibimos el mismo resultado
+  ELiminamos haciendo:
+  loans$debt_settlement_flag <- NULL
+- num_tl_30dpd: 0->114756     1->46      2->3 :Número de cuentas actualmente con 30 días de retraso (actualizado en los últimos 2 meses)
+  loans$num_tl_30dpd <- NULL
+- num_tl_120dpd_2m
+  loans$num_tl_120dpd_2m <- NULL
+- delinq_amnt
+  loans$delinq_amnt <- NULL
+- chargeoff_within_12_mths: table(loans$chargeoff_within_12_mths)
+             0      1      2      3      4      5
+        113972    780     40      5      7      1
+  loans$chargeoff_within_12_mths <- NULL
+- acc_now_delinq:
+            0      1      2      3
+       114727     74      3      1
+  loans$acc_now_delinq <- NULL
+- table(loans$next_pymnt_d)
+  Apr-2018 Feb-2018 Mar-2018
+      21       22   114734
+  loans$next_pymnt_d <- NULL
+- table(loans$recoveries)
+       0   2970
+  114804      1
+  loans$recoveries <- NULL
+- table(loans$total_rec_late_fee)
+       0     15  15.04  15.06
+  114350    166      1     2
+  loans$total_rec_late_fee <- NULL
+
+
+
+Encontramos que el siguiente valor solo tiene un único valor en todas sus columnas:
+- table(loans$policy_code)
+           1
+      114805
+  loans$policy_code <- NULL
+- table(loans$collection_recovery_fee)
+           0
+      114805
+  loans$collection_recovery_fee <- NULL
+
+
 pie(table(loans$tax_liens), title="test")
 table(table(loans$tax_liens))
 
