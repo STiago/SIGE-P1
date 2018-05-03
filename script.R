753b6f4a7b8198098a0581f98bed58ac6d28ff27
STiago <victoriasantiago@correo.ugr.es>
2018-05-03 01:25:25 +0100

    Adding decision trees - clasification rpart

--- script.R
+++ script.R
@@ -80,167 +80,123 @@ View(loans)
 
 # 3. Eliminación de variables sin información: Finding not relevant values
 
-Vemos que las 3 primeras columnas parecen iguales pero al aplicar el identical vemos que las dos que son iguales son las dos primeras:
+#Vemos que las 3 primeras columnas parecen iguales pero al aplicar el identical vemos que las dos que son iguales son las dos primeras:
 identical(loans$loan_amnt, loans$funded_amnt)
-Returns -> True
-identical(loans$funded_amnt, loans$funded_amnt_inv)
-Returns -> False
+#Returns -> True
 
-Asi que sabiendo que las dos primeras son iguales pasamos a eliminar una de ellas:
+identical(loans$funded_amnt, loans$funded_amnt_inv)
+#Returns -> False
+
+# Asi que sabiendo que las dos primeras son iguales pasamos a eliminar una de ellas:
 loans <- subset(loans, select= -funded_amnt)
 
-Ahora pasamos a explorar el contenido de las siguientes columnas y procedemos a eliminar las siguientes ya que no aportan informacion
-Valores muy desiqlibrados: eg: S->114600 N->3 ó  0->114690, 4->1, 13->2, 26->1
-- table(loans$hardship_flag): Donde sale que todas las filas no tienen este plan en su prestamos y solo un caso si N=114804      Y=1
-  loans$hardship_flag <- NULL
+#Ahora pasamos a explorar el contenido de las siguientes columnas y procedemos a eliminar las siguientes ya que no aportan informacion
+#Valores muy desequilibrados: eg: S->114600 N->3 ó  0->114690, 4->1, 13->2, 26->1
+table(loans$hardship_flag)#: Donde sale que todas las filas no tienen este plan en su prestamos y solo un caso si N=114804      Y=1
+loans$hardship_flag <- NULL
 
-- table(loans$debt_settlement_flag): Ocurre lo mismo que con el anterior y recibimos el mismo resultado
-  ELiminamos haciendo:
-  loans$debt_settlement_flag <- NULL
+table(loans$debt_settlement_flag) #: Ocurre lo mismo que con el anterior y recibimos el mismo resultado
+#ELiminamos haciendo:
+loans$debt_settlement_flag <- NULL
 
-- num_tl_30dpd: 0->114756     1->46      2->3 :Número de cuentas actualmente con 30 días de retraso (actualizado en los últimos 2 meses)
-  loans$num_tl_30dpd <- NULL
+table(num_tl_30dpd) #: 0->114756     1->46      2->3 :Número de cuentas actualmente con 30 días de retraso (actualizado en los últimos 2 meses)
+loans$num_tl_30dpd <- NULL
 
-- num_tl_120dpd_2m
-  loans$num_tl_120dpd_2m <- NULL
+table(num_tl_120dpd_2m)
+loans$num_tl_120dpd_2m <- NULL
 
-- delinq_amnt
-  loans$delinq_amnt <- NULL
+delinq_amnt
+loans$delinq_amnt <- NULL
 
-- chargeoff_within_12_mths: table(loans$chargeoff_within_12_mths)
-             0      1      2      3      4      5
-        113972    780     40      5      7      1
-  loans$chargeoff_within_12_mths <- NULL
+chargeoff_within_12_mths #: table(loans$chargeoff_within_12_mths)
+#             0      1      2      3      4      5
+#        113972    780     40      5      7      1
+loans$chargeoff_within_12_mths <- NULL
 
-- acc_now_delinq:
-            0      1      2      3
-       114727     74      3      1
-  loans$acc_now_delinq <- NULL
+acc_now_delinq
+#            0      1      2      3
+#       114727     74      3      1
+loans$acc_now_delinq <- NULL
 
-- table(loans$next_pymnt_d)
-  Apr-2018 Feb-2018 Mar-2018
-      21       22   114734
-  loans$next_pymnt_d <- NULL
+table(loans$next_pymnt_d)
+#  Apr-2018 Feb-2018 Mar-2018
+#      21       22   114734
+loans$next_pymnt_d <- NULL
 
-- table(loans$recoveries)
-       0   2970
-  114804      1
-  loans$recoveries <- NULL
+table(loans$recoveries)
+#       0   2970
+#  114804      1
+loans$recoveries <- NULL
 
-- table(loans$total_rec_late_fee)
-       0     15  15.04  15.06
-  114350    166      1     2
-  loans$total_rec_late_fee <- NULL
+table(loans$total_rec_late_fee)
+#       0     15  15.04  15.06
+#  114350    166      1     2
+loans$total_rec_late_fee <- NULL
 
-- table(loans$pymnt_plan)
-       n      y
-  114804      1
-  loans$pymnt_plan <- NULL
+table(loans$pymnt_plan)
+#       n      y
+#  114804      1
+loans$pymnt_plan <- NULL
 
 
 
-Encontramos que el siguiente valor solo tiene un único valor en todas sus columnas:
-- table(loans$policy_code)
-           1
-      114805
-  loans$policy_code <- NULL
-- table(loans$collection_recovery_fee)
-           0
-      114805
-  loans$collection_recovery_fee <- NULL
+#Encontramos que el siguiente valor solo tiene un único valor en todas sus columnas:
+table(loans$policy_code)
+#           1
+#      114805
+loans$policy_code <- NULL
+table(loans$collection_recovery_fee)
+#           0
+#      114805
+loans$collection_recovery_fee <- NULL
 
 
 dim(loans)
-[1] 114805     88
+#[1] 114805     88
 
 
-2. Transformacion
-Pasamos a transformar los valores de la columna "title" para compararla con los de la columna "purpose".
-- table(loans$title)
-            Business             Car financing          Credit card refinancing
-                1455                      1311                            24677
-  Debt consolidation                Green loan                      Home buying
-               60567                        72                             1345
-    Home improvement     Learning and training                   Major purchase
-                8341                         1                             3094
-    Medical expenses      Moving and relocation                           Other
-                1872                        862                           10387
-            Vacation
-                 821
-- table(loans$purpose)
-             car        credit_card  debt_consolidation        educational
-            1311              24676               60568                  1
-home_improvement              house      major_purchase            medical
-            8341               1345                3093               1873
-          moving              other    renewable_energy     small_business
-             862              10387                  72               1455
-        vacation
-             821
+#2. Transformacion
+#Pasamos a transformar los valores de la columna "title" para compararla con los de la columna "purpose".
+table(loans$title)
+#            Business             Car financing          Credit card refinancing
+#                1455                      1311                            24677
+#  Debt consolidation                Green loan                      Home buying
+#               60567                        72                             1345
+#    Home improvement     Learning and training                   Major purchase
+#                8341                         1                             3094
+#    Medical expenses      Moving and relocation                           Other
+#                1872                        862                           10387
+#            Vacation
+#                 821
 
-- Comparamos las columnas "title" y "purpose":
-Seleccionamos las dos columnas:
+table(loans$purpose)
+#             car        credit_card  debt_consolidation        educational
+#            1311              24676               60568                  1
+#home_improvement              house      major_purchase            medical
+#            8341               1345                3093               1873
+#          moving              other    renewable_energy     small_business
+#             862              10387                  72               1455
+#        vacation
+#             821
+
+# Comparamos las columnas "title" y "purpose":
+#Seleccionamos las dos columnas:
 title_purpose <- subset(loans, select=c("title", "purpose"))
-Ahora mostramos el numero de elementos distintos que tienen ambas columnas:
+
+#Ahora mostramos el numero de elementos distintos que tienen ambas columnas:
 length(unique(title_purpose$title))
-[1] 13
+#[1] 13
+
 length(unique(title_purpose$purpose))
-[1] 13
-A continuacion mostramos las graficas de sectores de ambos para compararlos
+#[1] 13
+
+#A continuacion mostramos las graficas de sectores de ambos para compararlos
 with(title_purpose, pie(table(title), labels=levels(title), xlab="", ylab="",  main="title", col=rainbow_hcl(13)))
 with(title_purpose, pie(table(purpose), labels=levels(purpose), xlab="",
   ylab="", main="purpose", col=rainbow_hcl(13)))
 
-Tambien mostramos la tabla donde tambien podemos apreciar claramente que se corresponden los valores:
-purpose
-title                       car credit_card debt_consolidation educational
-Business                    0           0                  0           0
-Car financing            1311           0                  0           0
-Credit card refinancing     0       24676                  1           0
-Debt consolidation          0           0              60567           0
-Green loan                  0           0                  0           0
-Home buying                 0           0                  0           0
-Home improvement            0           0                  0           0
-Learning and training       0           0                  0           1
-Major purchase              0           0                  0           0
-Medical expenses            0           0                  0           0
-Moving and relocation       0           0                  0           0
-Other                       0           0                  0           0
-Vacation                    0           0                  0           0
- purpose
-title                     home_improvement house major_purchase medical moving
-Business                               0     0              0       0      0
-Car financing                          0     0              0       0      0
-Credit card refinancing                0     0              0       0      0
-Debt consolidation                     0     0              0       0      0
-Green loan                             0     0              0       0      0
-Home buying                            0  1345              0       0      0
-Home improvement                    8341     0              0       0      0
-Learning and training                  0     0              0       0      0
-Major purchase                         0     0           3093       1      0
-Medical expenses                       0     0              0    1872      0
-Moving and relocation                  0     0              0       0    862
-Other                                  0     0              0       0      0
-Vacation                               0     0              0       0      0
- purpose
-title                     other renewable_energy small_business vacation
-Business                    0                0           1455        0
-Car financing               0                0              0        0
-Credit card refinancing     0                0              0        0
-Debt consolidation          0                0              0        0
-Green loan                  0               72              0        0
-Home buying                 0                0              0        0
-Home improvement            0                0              0        0
-Learning and training       0                0              0        0
-Major purchase              0                0              0        0
-Medical expenses            0                0              0        0
-Moving and relocation       0                0              0        0
-Other                   10387                0              0        0
-Vacation                    0                0              0      821
-
-
-
-Por lo que comparandolas podemos ver que se corresponden bastante bien.
-Aun asi, sabiendo ya que son la misma columna para estar mas seguros vamos a transformar los nombres de la columna tittle a los correspondientes nombres de la columna title_purpose.
+#Por lo que comparandolas podemos ver que se corresponden bastante bien.
+#Aun asi, sabiendo ya que son la misma columna para estar mas seguros vamos a transformar los nombres de la columna tittle a los correspondientes nombres de la columna title_purpose.
 
 
 
@@ -274,10 +230,10 @@ for (i in 1:length(loans$title)) {
   }
 }
 
-Ahora eliminamos la columna title ya que solo se diferencia de en 2 valores con la columna purpose y no estan escritos bien los nombres para su tratamiento.
+#Ahora eliminamos la columna title ya que solo se diferencia de en 2 valores con la columna purpose y no estan escritos bien los nombres para su tratamiento.
 loans$title <- NULL
-> dim(loans)
-[1] 114805     87
+dim(loans)
+#[1] 114805     87
 
 
 ### Correlacion por cada par de variables
@@ -362,7 +318,6 @@ for(i in names(loans2)){
 
 # Obtenemos los coeficientes de correlacion entre todas las variables
 val_cor <- cor(loans2)
-#####
 
 
 # Decision tree
@@ -393,8 +348,8 @@ summary(fit)
 #              27               11               11                9                9                6
 #   total_rec_int      total_pymnt  total_pymnt_inv  total_rec_prncp         int_rate       addr_state
 #               5                5                5                5                2                1
-#       sub_grade       emp_length
-#               1                1
+#       sub_grade       emp_length   purpose
+#               1                1         1
 
 
 # Random Forest
@@ -435,7 +390,7 @@ names(loans6.important.numeric)
 # Añado la columna loan_status a loans6.important.numeric antes de aplicar random Forest
 loans6.important.numeric$loan_status <- loans6$loan_status
 
-# Random fores para quedarnos con las variables numericas mas importantes
+# Random forest para quedarnos con las variables numericas mas importantes
 rf.model <- randomForest(loans6.important.numeric$loan_status ~.,
             data = loans6.important.numeric,
             ntree = 35,
@@ -466,8 +421,123 @@ importancias2 = varimp(rf.model2, conditional = TRUE)
 library(varSelRF)
 rf.model3 <- randomForest(loans6.important.numeric$loan_status~.,data=loans6.important.numeric,
 ntree=35,importance=TRUE,maxnodes=10,mtry=25)
-importancia3 <- randomVarImpsRF(xdata=loans6.important.numeric[,-177],Class=loans6.important.numeric$loan_status,
-forest=rf.model3,usingCluster = FALSE)
+
+importancia3 <- randomVarImpsRF(xdata=loans6.important.numeric[,-15],Class=loans6.important.numeric$loan_status, forest=rf.model3,usingCluster = FALSE)
+
+# Obtaining random importances
+
+importancia3
+                                       1            2             3             4             5
+#out_prncp_inv               3.773840e-03 5.289862e-03  3.818486e-03  5.659796e-03  3.287021e-03
+#total_pymnt_inv             5.904601e-03 1.256743e-02  4.869401e-03  9.304816e-03  1.091554e-02
+#total_rec_prncp             4.331203e-03 8.413556e-03  3.374315e-03  7.300451e-03  7.292460e-03
+#total_rec_int               5.492075e-03 4.429881e-03  4.185919e-03  6.874625e-03  4.425017e-03
+#last_pymnt_amnt             5.253064e-03 6.186696e-03  4.561308e-03  8.877544e-03  5.005609e-03
+#collections_12_mths_ex_med -2.235835e-05 4.190424e-06 -1.063458e-05 -1.918357e-05  1.067252e-05
+#tot_coll_amt                7.220748e-05 1.517099e-04  7.653916e-05  3.103355e-05  2.554096e-05
+#open_acc_6m                 4.596849e-04 3.239543e-04  5.486449e-04  4.106257e-04  4.496276e-04
+#open_act_il                 1.255622e-03 1.238304e-03  1.493015e-03  1.279740e-03  6.782167e-04
+#open_il_12m                 2.734292e-04 3.467248e-04  4.867280e-04  4.168041e-04  2.092024e-04
+
+# Clasificacion
+
+varia_loans <- c('loans_status',
+                 'emp_title',
+                  'purpose',
+                  'verification_status',
+                  'revol_util',
+                  'last_pymnt_d',
+                  'zip_code',
+                  'last_pymnt_amnt',
+                  'total_rec_int',
+                  'total_pymnt',
+                  'total_pymnt_inv',
+                  'total_rec_prncp',
+                  'int_rate',
+                  'sub_grade',
+                  'emp_length')
+
+#Creamos uno nuevo unicamente con las variables que nos interesan de los anteriores preprocesamientos
+#loans7 <- loans5
+#final_loan = data.frame()
+#for(i in names(loans7)){
+#    for(j in varia_loans){
+#        if(i==j){
+#            elem <- c(loans7[,(i)])
+#        }
+#    }
+#}
+#final_loan <- data.frame(elem)
+#Añadimos la variable loans_status
+#final_loan$loans_status <- loans6$loan_status
+
+loans_status <-loans6$loan_status
+emp_title <- loans5$emp_title
+purpose <- loans5$purpose
+verification_status <- loans5$verification_status
+revol_util <- loans5$revol_util
+last_pymnt_d <- loans5$last_pymnt_d
+zip_code <- loans5$zip_code
+last_pymnt_amnt <- loans5$last_pymnt_amnt
+total_rec_int  <- loans5$total_rec_int
+total_pymnt <- loans5$total_pymnt
+total_pymnt_inv <- loans5$total_pymnt_inv
+total_rec_prncp  <- loans5$total_rec_prncp
+int_rate <- loans5$int_rate
+sub_grade <- loans5$sub_grade
+emp_length <- loans5$emp_length
+
+final_loans <- data.frame(loans_status,sub_grade, purpose, verification_status, revol_util, last_pymnt_d, zip_code, last_pymnt_amnt, total_rec_int, total_pymnt, total_pymnt_inv, total_rec_prncp,
+int_rate,  emp_title, emp_length)
+
+
+
+# Dividimos el dataset en dos partes
+set.seed(1234)
+ind <- sample(2, nrow(final_loan), replace = TRUE, prob = c(0.7, 0.3))
+entrenamiento <- final_loan[ind == 1, ]
+validacion <- final_loan[ind == 2, ]
+
+# R-part
+# Modelo
+my_form <- loans_status ~ emp_title + purpose + verification_status + revol_util + last_pymnt_d + zip_code + last_pymnt_amnt + total_rec_int + total_pymnt + total_pymnt_inv + total_rec_prncp + int_rate + sub_grade + emp_length
+#my_form <- loans_status ~ purpose + verification_status + last_pymnt_d + zip_code + last_pymnt_amnt + total_rec_int + total_pymnt + total_pymnt_inv + total_rec_prncp + int_rate + sub_grade + emp_length
+fit_train <- rpart(my_form, data=entrenamiento, method="class")
+
+# Vemos el grafico del arbol de decision
+fancyRpartPlot(fit_train)
+
+# Prediccion
+library(rpart)
+library(rattle)
+library(ROCR)
+
+predict.rpart <- predict(fit_train,validacion,type = "prob")[,2] #prob. clase=yes
+predict.rocr  <- prediction (predict.rpart,validacion$loans_status)
+perf.rocr     <- performance(predict.rocr,"tpr","fpr") #True y False postivie.rate
+
+# Ahora vemos nuestro grafico de la curva ROC
+auc <- as.numeric(performance(predict.rocr ,"auc")@y.values)
+plot(perf.rocr,type='o', main = paste('Area Bajo la Curva =',round(auc,2)))
+abline(a=0, b= 1)
+
+
+# Random forest
+rf.model_train <- randomForest(entrenamiento$loan_status ~.,
+            data = entrenamiento,
+            ntree = 1000,
+            type="classification",
+            importance=TRUE,
+            na.action=na.omit)
+
+rf.model_train <- randomForest(entrenamiento$loans_status ~ entrenamiento$purpose + entrenamiento$sub_grade + entrenamiento$verification_status + entrenamiento$last_pymnt_d +entrenamiento$last_pymnt_amnt, data = entrenamiento,
+ntree = 1000,
+type="classification",
+importance=TRUE,
+na.action=na.omit)
+
+varImpPlot(rf.model_train)
+getTree(rf.model_train, 1)
 
 
 
